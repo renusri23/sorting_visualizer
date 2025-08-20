956f3ca65165f11e2463577ddadfb2f00c220d5a
renusri23 <renusri.duggirala123@gmail.com>
2025-08-20 23:58:31 +0530

    removed unused files

--- src/utils/generateArray.js
+++
@@ -1,7 +0,0 @@
-export default function generateArray(size = 20, max = 30) {
-  let arr = [];
-  for (let i = 0; i < size; i++) {
-    arr.push(Math.floor(Math.random() * max) + 1);
-  }
-  return arr;
-}
